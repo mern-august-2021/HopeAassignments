9222e32bc8cddc47ddb5a78700ce57379c7c2646
HAnderson9933 <79612554+HAnderson9933@users.noreply.github.com>
2021-08-11 21:37:12 -0600

    Hook Form

---
+++ 2_react/2_functional_components/hookform/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UserForm from './componenets/UserFrom';
+
+function App() {
+  return (
+    <div className="App">
+      <UserForm/>
+    </div>
+  );
+}
+
+export default App;
